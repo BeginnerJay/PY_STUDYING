2ba09a4cd9d166d7ac2851dcf6a3747e11f07a78
BeginnerJay <febukyongira@gmail.com>
2019-04-18 15:40:23 +0900

    Learned unsupervised learning(Chapter 03)

--- PyLibML/cache/03-unsupervised-learning.py
+++ PyLibML/cache/03-unsupervised-learning.py
@@ -508,6 +508,479 @@ for i in range(len(digits.data)):
 plt.xlabel("t-SNE 특성 0")
 plt.ylabel("t-SNE 특성 1")
 
+#### 3.5 군집
+    ### 3.5.1 k-평균 군집
+        ## 이 알고리즘은 데이터의 어떤 영역을 대표하는 클러스터 중싐을 찾는다.
+            # 먼저 데이터 포인트를 가장 가까운 클러스터 중심에 할당
+            # 그 후 클러스터에 할당된 데이터 포인트의 평균으로 클러스터 중심 재지정
+        ## dataset의 cluster 갯수 정확히 알아도 k-mean 알고리즘이 구분 못할수 있음.
+            # k-mean은 모든 클러스터의 반경이 똑같다고 가정.
+            # 복잡한 형태라면 k-mean의 성능이 더 나빠진다.
+        ## 중심으로 각 데이터 포인트를 표현
+            # 각 data point가 하나의 cluster 중심, 즉 하나의 성분으로 표현된다고 본다.
+            # 이렇게 각 포인트가 하나의 성분으로 분해되는 관점으로 보는 것 -> 벡터 양자화(vector quantization)
+            # 벡터 양자화의 흥미로운 점 -> 데이터의 차원보다 더 많은 클러스터 써서 data encoding 가능
+                # 차원을 높여서 더 복잡한 형태의 data도 군집 가능
+        ## 단점
+            # 무작위 초기화 사용 -> algorithm output이 초기 난수에 따라 달라짐.
+            # 클러스터의 모양을 미리 가정 -> 활용 범위 제한적
+            # 찾으려 하는 클러스터 갯수 지정 필요
+    ### 군집은 각 데이터 포인트가 레이블을 가진다는 점에서는 분류와 비슷하다.
+        ## 그러나 정답을 모르고 있으며, 레이블 자체에 어떤 의미가 있는 것은 아니다.
+            # 클러스터 n에는 한 사람의 얼굴만 담겨 있을 수 있다.
+            # 하지만 이는 사진을 직접 봐야 알 수 있으며, 숫자 n은 아무런 의미가 없다.
+            # n이라고 labeled된 얼굴들은 모두 어떤 점에서 비슷하다는 것 뿐이다.
+    ### 3.5.2 병합 군집
+        ## 병학 군집 알고리즘은 시작할 때 각 포인트를 하나의 클러스토로 지정
+        ## 그 후 어떤 조건을 만족할 때까지 가장 비슷한 두 클러스터를 합쳐나감.
+        ## scikit-learn
+            # ward : 클러스터 내의 분산 가장 적게 증가시키는 두 클러스터 합침
+            # average : 평균 거리가 가장 짧은 두 클러스터 합침
+            # complete : 클러스터 포인트 사이의 최대 거리 가장 짧은 둘 합침
+        ## 알고리즘 특성상 세로운 데이터 포인트에 대해 예측 불가능
+            # 병합 군집은 predict 메서드 대신 fit_predict 메서드 사용
+        ## 계층적 군집과 덴드로그램
+            # 군집 반복 -> 포인트 1개짜리 클러스터에서 마지막 클러스터까지 이동
+            # 덴드로그램으로 2차원 이상의 데이터셋도 시각화 가능
+    ### 3.5.3 DBSCAN
+        ## 클러스터의 개수를 미리 지정할 필요가 없음
+        ## 다소 느리지만, 큰 데이터셋에도 적용 간응
+        ## 특성 공간에서 가까이 있는 데잍가 많이 붐비는 지역의 포인트를 찾는다.
+            # 이를 특성 공간의 dense region이라고 한다.
+            # 비어 있는 지역을 경계로 다른 클러스터와 구분된다.
+        ## 무작위로 포인트를 선택
+            # eps 거리 안에 있는 모든 포인트 찾기.
+            # eps 안에 있는 포인트 수가 min_sample 수보다 적음 -> 잡음으로 레이블
+            # min_sample 수보다 많음 -> 핵심 샘플로 레이블 -> 거리 안의 모든 이웃 보기
+            # 어떤 클러스터에도 아직 할당 안된 것 有 -> 바로 전에 만든 label allocate
+        ## DBSCAN을 한 dataset에 다회 실행 -> 핵심 포인트 군집, 잡음 항상 같음.
+        ## 포인트 순서에 의해 받는 영향은 적다.
+        ## min_sample 설정은, 덜 조밀한 지역에 있는 포인트들이
+            # 잡음이 될지 클러스터가 될지를 결정하는데 중요한 역할을 한다.
+        ## cluster 갯수 지정 필요는 없지만, eps 값은 간접적으로 cluster 갯수를 제어.
+            # 적절한 eps 찾으려면 StandardScaler나 MinMaxScaler로 조정 필요
+            # eps 내리면 너무 많은 클러스터 생성할수도 있음.
+            # 클러스터 할당 
+# 값을 주의해서 다룰 것!
+    ### 3.5.4 군집 알고리즘의 비교와 평가
+        ## 알고리즘이 잘 되는지, 혹은 비교하기가 어려움.
+        ## 타깃값으로 군집 평가하기
+            # ARI : Adjusted rand index(음수 가능하긴 함)
+            # NMI : normalized mutual information
+        ## ARI나 NMI 같은 군집용 측정 도구를 사용 않고, 정확도 측정하면 안된다!
+            # 정확도 쓰면 할당된 클러스터 레이블이 실제 레이블과 맞는지 확인한다.
+            # 하지만 cluster label은 그 자체로는 의미가 없다.
+            # 포인트들이 같은 클러스터에 속해 있는지만이 중요할 뿐이다.
+        ## 타깃값 없이 군집 평가하기
+            # 타깃 값이 있다면 분류기를 만들면 된다.
+            # 실루엣 계수 : 효과 떨어짐.
+            # 견고성 기반 지표 사용
+
+
+### KMeans의 객체를 생성하고 찾는 클러스터의 수를 지정한다. 그 후 fit method 호출
+
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+
+# 인위적으로 2차원 데이터를 생성합니다
+X, y = make_blobs(random_state=1)
+# 군집 모델을 만듭니다
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(X)
+print(kmeans.labels_)
+print(kmeans.predict(X))
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+# 두 개의 클러스터 중심을 사용합니다
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(X)
+assignments = kmeans.labels_
+# 다섯 개의 클러스터 중심을 사용합니다
+kmeans = KMeans(n_clusters=5)
+kmeans.fit(X)
+assignments = kmeans.labels_
+
+X_varied, y_varied = make_blobs(n_samples=200,
+                                cluster_std=[1.0, 2.5, 0.5],
+                                random_state=170)
+y_pred = KMeans(n_clusters=3, random_state=0).fit_predict(X_varied)
+
+mglearn.discrete_scatter(X_varied[:, 0], X_varied[:, 1], y_pred)
+plt.legend(["클러스터 0", "클러스터 1", "클러스터 2"], loc='best')
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+# 무작위로 클러스터 데이터 생성합니다
+X, y = make_blobs(random_state=170, n_samples=600)
+rng = np.random.RandomState(74)
+
+# 데이터가 길게 늘어지도록 변경합니다
+transformation = rng.normal(size=(2, 2))
+X = np.dot(X, transformation)
+
+# 세 개의 클러스터로 데이터에 KMeans 알고리즘을 적용합니다
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(X)
+y_pred = kmeans.predict(X)
+
+# 클러스터 할당과 클러스터 중심을 나타냅니다
+mglearn.discrete_scatter(X[:, 0], X[:, 1], kmeans.labels_, markers='o')
+mglearn.discrete_scatter(
+    kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], [0, 1, 2],
+    markers='^', markeredgewidth=2)
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+# two_moons 데이터를 생성합니다(이번에는 노이즈를 조금만 넣습니다)
+from sklearn.datasets import make_moons
+X, y = make_moons(n_samples=200, noise=0.05, random_state=0)
+
+# 두 개의 클러스터로 데이터에 KMeans 알고리즘을 적용합니다
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(X)
+y_pred = kmeans.predict(X)
+
+# 클러스터 할당과 클러스터 중심을 표시합니다
+plt.scatter(X[:, 0], X[:, 1], c=y_pred, cmap=mglearn.cm2, s=60, edgecolors='k')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1],
+            marker='^', c=[mglearn.cm2(0), mglearn.cm2(1)], s=100, linewidth=2, edgecolors='k')
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+X_train, X_test, y_train, y_test = train_test_split(
+    X_people, y_people, stratify=y_people, random_state=42)
+nmf = NMF(n_components=100, random_state=0)
+nmf.fit(X_train)
+pca = PCA(n_components=100, random_state=0)
+pca.fit(X_train)
+kmeans = KMeans(n_clusters=100, random_state=0)
+kmeans.fit(X_train)
+
+X_reconstructed_pca = pca.inverse_transform(pca.transform(X_test))
+X_reconstructed_kmeans = kmeans.cluster_centers_[kmeans.predict(X_test)]
+X_reconstructed_nmf = np.dot(nmf.transform(X_test), nmf.components_)
+
+fig, axes = plt.subplots(3, 5, figsize=(8, 8), subplot_kw={'xticks': (), 'yticks': ()})
+fig.suptitle("추출한 성분")
+for ax, comp_kmeans, comp_pca, comp_nmf in zip(
+        axes.T, kmeans.cluster_centers_, pca.components_, nmf.components_):
+    ax[0].imshow(comp_kmeans.reshape(image_shape))
+    ax[1].imshow(comp_pca.reshape(image_shape), cmap='viridis')
+    ax[2].imshow(comp_nmf.reshape(image_shape))
+
+axes[0, 0].set_ylabel("kmeans")
+axes[1, 0].set_ylabel("pca")
+axes[2, 0].set_ylabel("nmf")
+
+fig, axes = plt.subplots(4, 5, subplot_kw={'xticks': (), 'yticks': ()},
+                         figsize=(8, 8))
+fig.suptitle("재구성")
+for ax, orig, rec_kmeans, rec_pca, rec_nmf in zip(
+        axes.T, X_test, X_reconstructed_kmeans, X_reconstructed_pca,
+        X_reconstructed_nmf):
+    ax[0].imshow(orig.reshape(image_shape))
+    ax[1].imshow(rec_kmeans.reshape(image_shape))
+    ax[2].imshow(rec_pca.reshape(image_shape))
+    ax[3].imshow(rec_nmf.reshape(image_shape))
+
+axes[0, 0].set_ylabel("원본")
+axes[1, 0].set_ylabel("kmeans")
+axes[2, 0].set_ylabel("pca")
+axes[3, 0].set_ylabel("nmf")
+
+X, y = make_moons(n_samples=200, noise=0.05, random_state=0)
+
+kmeans = KMeans(n_clusters=10, random_state=0)
+kmeans.fit(X)
+y_pred = kmeans.predict(X)
+
+plt.scatter(X[:, 0], X[:, 1], c=y_pred, s=60, cmap='Paired', edgecolors='black')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=60,
+            marker='^', c=range(kmeans.n_clusters), linewidth=2, cmap='Paired', edgecolors='black')
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+print("클러스터 레이블:\n", y_pred)
+
+distance_features = kmeans.transform(X)
+print("클러스터 거리 데이터의 형태:", distance_features.shape)
+print("클러스터 거리:\n", distance_features)
+
+### 3.5.2 병합 군집
+
+from sklearn.cluster import AgglomerativeClustering
+X, y = make_blobs(random_state=1)
+
+agg = AgglomerativeClustering(n_clusters=3)
+assignment = agg.fit_predict(X)
+
+mglearn.discrete_scatter(X[:, 0], X[:, 1], assignment)
+plt.legend(["클러스터 0", "클러스터 1", "클러스터 2"], loc="best")
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+
+# SciPy에서 ward 군집 함수와 덴드로그램 함수를 임포트합니다
+from scipy.cluster.hierarchy import dendrogram, ward
+
+X, y = make_blobs(random_state=0, n_samples=12)
+# 데이터 배열 X 에 ward 함수를 적용합니다
+# SciPy의 ward 함수는 병합 군집을 수행할 때 생성된
+# 거리 정보가 담긴 배열을 리턴합니다
+linkage_array = ward(X)
+# 클러스터 간의 거리 정보가 담긴 linkage_array를 사용해 덴드로그램을 그립니다
+dendrogram(linkage_array)
+
+# 두 개와 세 개의 클러스터를 구분하는 커트라인을 표시합니다
+ax = plt.gca()
+bounds = ax.get_xbound()
+ax.plot(bounds, [7.25, 7.25], '--', c='k')
+ax.plot(bounds, [4, 4], '--', c='k')
+
+ax.text(bounds[1], 7.25, ' 두 개 클러스터', va='center', fontdict={'size': 15})
+ax.text(bounds[1], 4, ' 세 개 클러스터', va='center', fontdict={'size': 15})
+plt.xlabel("샘플 번호")
+plt.ylabel("클러스터 거리")
+
+### 3.5.3 DBSCAN
+
+### 타깃값으로 군집 평가하기
+from sklearn.metrics.cluster import adjusted_rand_score
+X, y = make_moons(n_samples=200, noise=0.05, random_state=0)
+
+# 평균이 0, 분산이 1이 되도록 데이터의 스케일을 조정합니다
+scaler = StandardScaler()
+scaler.fit(X)
+X_scaled = scaler.transform(X)
+
+fig, axes = plt.subplots(1, 4, figsize=(15, 3),
+                         subplot_kw={'xticks': (), 'yticks': ()})
+
+# 사용할 알고리즘 모델을 리스트로 만듭니다
+algorithms = [KMeans(n_clusters=2), AgglomerativeClustering(n_clusters=2),
+              DBSCAN()]
+
+# 비교를 위해 무작위로 클러스터 할당을 합니다
+random_state = np.random.RandomState(seed=0)
+random_clusters = random_state.randint(low=0, high=2, size=len(X))
+
+# 무작위 할당한 클러스터를 그립니다
+axes[0].scatter(X_scaled[:, 0], X_scaled[:, 1], c=random_clusters,
+                cmap=mglearn.cm3, s=60, edgecolors='black')
+axes[0].set_title("무작위 할당 - ARI: {:.2f}".format(
+        adjusted_rand_score(y, random_clusters)))
+
+for ax, algorithm in zip(axes[1:], algorithms):
+    # 클러스터 할당과 클러스터 중심을 그립니다
+    clusters = algorithm.fit_predict(X_scaled)
+    ax.scatter(X_scaled[:, 0], X_scaled[:, 1], c=clusters,
+               cmap=mglearn.cm3, s=60, edgecolors='black')
+    ax.set_title("{} - ARI: {:.2f}".format(algorithm.__class__.__name__,
+                                           adjusted_rand_score(y, clusters)))
+
+from sklearn.metrics import accuracy_score
+
+    # 포인트가 클러스터로 나뉜 두 가지 경우
+clusters1 = [0, 0, 1, 1, 0]
+clusters2 = [1, 1, 0, 0, 1]
+    # 모든 레이블이 달라졌으므로 정확도는 0입니다
+print("정확도: {:.2f}".format(accuracy_score(clusters1, clusters2)))
+    # 같은 포인트가 클러스터에 모였으므로 ARI는 1입니다
+print("ARI: {:.2f}".format(adjusted_rand_score(clusters1, clusters2)))
+
+### 타깃 값 없이 군집 평가하기
+from sklearn.metrics.cluster import silhouette_score
+
+X, y = make_moons(n_samples=200, noise=0.05, random_state=0)
+
+# 평균이 0, 분산이 1이 되도록 데이터의 스케일을 조정합니다
+scaler = StandardScaler()
+scaler.fit(X)
+X_scaled = scaler.transform(X)
+
+fig, axes = plt.subplots(1, 4, figsize=(15, 3),
+                         subplot_kw={'xticks': (), 'yticks': ()})
+
+# 비교를 위해 무작위로 클러스터 할당을 합니다
+random_state = np.random.RandomState(seed=0)
+random_clusters = random_state.randint(low=0, high=2, size=len(X))
+
+# 무작위 할당한 클러스터를 그립니다
+axes[0].scatter(X_scaled[:, 0], X_scaled[:, 1], c=random_clusters,
+                cmap=mglearn.cm3, s=60, edgecolors='black')
+axes[0].set_title("무작위 할당: {:.2f}".format(
+        silhouette_score(X_scaled, random_clusters)))
+
+algorithms = [KMeans(n_clusters=2), AgglomerativeClustering(n_clusters=2),
+              DBSCAN()]
+
+for ax, algorithm in zip(axes[1:], algorithms):
+    clusters = algorithm.fit_predict(X_scaled)
+    # 클러스터 할당과 클러스터 중심을 그립니다
+    ax.scatter(X_scaled[:, 0], X_scaled[:, 1], c=clusters, cmap=mglearn.cm3,
+               s=60, edgecolors='black')
+    ax.set_title("{} : {:.2f}".format(algorithm.__class__.__name__,
+                                      silhouette_score(X_scaled, clusters)))
+    
+### 얼굴 데이터셋으로 군집 알고리즘 비교
+
+# LFW 데이터에서 고유얼굴을 찾은 다음 데이터를 변환합니다
+from sklearn.decomposition import PCA
+pca = PCA(n_components=100, whiten=True, random_state=0)
+X_pca = pca.fit_transform(X_people)
+
+## DBSCAN으로 얼굴 데이터셋 분석
+
+# 기본 매개변수로 DBSCAN을 적용합니다
+dbscan = DBSCAN()
+labels = dbscan.fit_predict(X_pca)
+print("고유한 레이블:", np.unique(labels))
+
+
+dbscan = DBSCAN(min_samples=3)
+labels = dbscan.fit_predict(X_pca)
+print("고유한 레이블:", np.unique(labels))
+
+dbscan = DBSCAN(min_samples=3, eps=15)
+labels = dbscan.fit_predict(X_pca)
+print("고유한 레이블:", np.unique(labels))
+
+
+# 잡음 포인트와 클러스터에 속한 포인트 수를 셉니다.
+# bincount는 음수를 받을 수 없어서 labels에 1을 더했습니다.
+# 반환값의 첫 번째 원소는 잡음 포인트의 수입니다.
+print("클러스터별 포인트 수:", np.bincount(labels + 1))
+
+noise = X_people[labels==-1]
+
+fig, axes = plt.subplots(3, 9, subplot_kw={'xticks': (), 'yticks': ()},
+                         figsize=(12, 4))
+for image, ax in zip(noise, axes.ravel()):
+    ax.imshow(image.reshape(image_shape), vmin=0, vmax=1)
+
+for eps in [1, 3, 5, 7, 9, 11, 13]:
+    print("\neps=", eps)
+    dbscan = DBSCAN(eps=eps, min_samples=3)
+    labels = dbscan.fit_predict(X_pca)
+    print("클러스터 수:", len(np.unique(labels)))
+    print("클러스터 크기:", np.bincount(labels + 1))
+
+dbscan = DBSCAN(min_samples=3, eps=7)
+labels = dbscan.fit_predict(X_pca)
+
+for cluster in range(max(labels) + 1):
+    mask = labels == cluster
+    n_images =  np.sum(mask)
+    fig, axes = plt.subplots(1, 14, figsize=(14*1.5, 4),
+                             subplot_kw={'xticks': (), 'yticks': ()})
+    i = 0
+    for image, label, ax in zip(X_people[mask], y_people[mask], axes):
+        ax.imshow(image.reshape(image_shape), vmin=0, vmax=1)
+        ax.set_title(people.target_names[label].split()[-1])
+        i += 1
+    for j in range(len(axes) - i):
+        axes[j+i].imshow(np.array([[1]*65]*87), vmin=0, vmax=1)
+        axes[j+i].axis('off')
+
+## k-평균으로 얼굴 데이터셋 분석하기
+
+n_clusters = 10
+# k-평균으로 클러스터를 추출합니다
+km = KMeans(n_clusters=n_clusters, random_state=0)
+labels_km = km.fit_predict(X_pca)
+print("k-평균의 클러스터 크기:", np.bincount(labels_km))
+
+fig, axes = plt.subplots(2, 5, subplot_kw={'xticks': (), 'yticks': ()},
+                         figsize=(12, 4))
+for center, ax in zip(km.cluster_centers_, axes.ravel()):
+    ax.imshow(pca.inverse_transform(center).reshape(image_shape),
+              vmin=0, vmax=1)
+
+mglearn.plots.plot_kmeans_faces(km, pca, X_pca, X_people,
+                                y_people, people.target_names)
+
+## 병합 군집으로 얼굴 데이터셋 분석하기
+
+# 병합 군집으로 클러스터를 추출합니다
+agglomerative = AgglomerativeClustering(n_clusters=10)
+labels_agg = agglomerative.fit_predict(X_pca)
+print("병합 군집의 클러스터 크기:",
+       np.bincount(labels_agg))
+
+print("ARI: {:.2f}".format(adjusted_rand_score(labels_agg, labels_km)))
+
+linkage_array = ward(X_pca)
+# 클러스터 사이의 거리가 담겨있는 linkage_array로 덴드로그램을 그립니다
+plt.figure(figsize=(20, 5))
+dendrogram(linkage_array, p=7, truncate_mode='level', no_labels=True)
+plt.xlabel("샘플 번호")
+plt.ylabel("클러스터 거리")
+ax = plt.gca()
+bounds = ax.get_xbound()
+ax.plot(bounds, [36, 36], '--', c='k')
+
+n_clusters = 10
+for cluster in range(n_clusters):
+    mask = labels_agg == cluster
+    fig, axes = plt.subplots(1, 10, subplot_kw={'xticks': (), 'yticks': ()},
+                             figsize=(15, 8))
+    axes[0].set_ylabel(np.sum(mask))
+    for image, label, asdf, ax in zip(X_people[mask], y_people[mask],
+                                      labels_agg[mask], axes):
+        ax.imshow(image.reshape(image_shape), vmin=0, vmax=1)
+        ax.set_title(people.target_names[label].split()[-1],
+                     fontdict={'fontsize': 9})
+
+# 병합 군집으로 클러스터를 추출합니다
+agglomerative = AgglomerativeClustering(n_clusters=40)
+labels_agg = agglomerative.fit_predict(X_pca)
+print("병합 군집의 클러스터 크기:", np.bincount(labels_agg))
+
+n_clusters = 40
+for cluster in [13, 16, 23, 38, 39]: # 흥미로운 클러스터 몇개를 골랐습니다
+    mask = labels_agg == cluster
+    fig, axes = plt.subplots(1, 15, subplot_kw={'xticks': (), 'yticks': ()},
+                             figsize=(15, 8))
+    cluster_size = np.sum(mask)
+    axes[0].set_ylabel("#{}: {}".format(cluster, cluster_size))
+    for image, label, asdf, ax in zip(X_people[mask], y_people[mask],
+                                      labels_agg[mask], axes):
+        ax.imshow(image.reshape(image_shape), vmin=0, vmax=1)
+        ax.set_title(people.target_names[label].split()[-1],
+                     fontdict={'fontsize': 9})
+    for i in range(cluster_size, 15):
+        axes[i].set_visible(False)
+
+### 요약 및 정리
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
